ef6863ba21cd68c1f738d45b4636358f7c49631d
mothball <mothball@users.noreply.github.com>
2022-02-24 18:04:36 +0000

    Added gitignore and function docstrings

--- Spring 2022/PHYS 780/hw1.py
+++ Spring 2022/PHYS 780/hw1.py
@@ -2,26 +2,83 @@ import numpy as np
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 
+
 def calc_force(r):
+    """
+    Calculate force based on distance
+
+    Parameters
+    ----------
+    r : float
+        distance between particles.
+
+    Returns
+    -------
+    float
+    """
     return 24*(2*np.power(r, -13) - np.power(r, -7))
 
 
 def calc_position(r_prev, p_prev, dt):
+    """
+    Calculate new position
+
+    Parameters
+    ----------
+    r_prev : float
+        previous particle distance.
+    p_prev : float
+        previous particle momentum.
+    dt : float
+        time step.
+
+    Returns
+    -------
+    float
+        new particle position.
+    """
     return r_prev + p_prev*dt + 0.5*calc_force(r_prev)*dt**2
 
 
 def calc_momentum(p_prev, f, f_prev, dt):
+    """
+    Calculate new momentum
+
+    Parameters
+    ----------
+    p_prev : float
+        previous particle momentum.
+    f : float
+        current force on particle.
+    f_prev : float
+        previous force on particle.
+    dt : float
+        time step.
+
+    Returns
+    -------
+    float
+        new particle momentm.
+    """
     return p_prev + 0.5*(f + f_prev)*dt
 
 
 def plot_data(x, y, x_axis_title='', y_axis_title='', title=''):
+    """
+    Plot data
+    """
     fig = go.Figure(data=go.Scatter(
         x=x,
         y=y,
-        mode='lines',
-        title=title
+        mode='lines'
         ))
 
+    fig.update_layout(
+    title={
+        'text': title,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'})
     fig.update_xaxes(title_text=x_axis_title)
     fig.update_yaxes(title_text=y_axis_title)
 
@@ -60,7 +117,7 @@ print(U_avg)
 # Can make dt and n_final adjustable
 # Should be able enter any value for r0, p0
 
-# plot_data(t, r, x_axis_title='time', y_axis_title='position')
+plot_data(t, r, x_axis_title='time', y_axis_title='position', title='Position vs. Time')
 # plot_data(t, p, x_axis_title='time', y_axis_title='momenum')
 # plot_data(t, F, x_axis_title='time', y_axis_title='force')
 # plot_data(t, K, x_axis_title='time', y_axis_title='force')
